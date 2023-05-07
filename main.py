4c09a753a3b94919afe1921e40efbceeed1e3657
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-05-07 14:18:28 +0330

    auto add import statments

--- main.py
+++ main.py
@@ -6,26 +6,22 @@ my_files = glob.glob(os.path.abspath(os.getcwd()) + r'/**/**/**/*.java', recursi
 my_files = list(dict.fromkeys(my_files))
 main = ''
 
-import_list = [
-    'import java.util.ArrayList;',
-    'import java.util.Arrays;',
-    'import java.util.Objects;',
-    'import java.util.Scanner;',
-    "import java.util.*;",
-    "import java.util.regex.Matcher;",
-    "import java.util.regex.Pattern;", ]
+import_list = []
 
 for item in my_files:
     with open(os.path.relpath(item)) as file:
-        aa = re.split(r'public', file.read(), 1)
+        split_class = re.split(r'public ', file.read(), 1)
+        import_block = split_class[0].split("\n")
+        import_list += [item for item in import_block if item.startswith("import")]
         temp = ""
         i = 0
-        for a in aa:
+        for a in split_class:
             if i != 0:
                 temp += a
             i += 1
         main += "\n\n" + temp
 with open("Main.java", 'w') as file:
+    file.write("")
     import_list  = "\n".join(import_list)
     main = import_list + main
     file.write(main)
